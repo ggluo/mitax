329e23de8b9d82b6beca2cad26bf8de5f9489011
luo9 <guanxiong.luo@med.uni-goettingen.de>
2023-12-11 12:35:59 +0100

    replace abs path with relative path

--- examples/mnist_classifier.py
+++ examples/mnist_classifier.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('/home/gluo/mitax')
-
 from mitax.trainer import trainer
 from mitax.misc import utils
 
@@ -16,7 +13,7 @@ os.environ['CUDA_VISIBLE_DEVICES']='3'
 def map_f(x):
     return x[0][..., np.newaxis], x[1]
 
-config = utils.load_config('/home/gluo/mitax/examples/mnist.yaml')
+config = utils.load_config('./mnist.yaml')
 
 d1 = Mnist('train', True, './')
 train_pipe = utils.dataloader(d1, config['num_thread'], map_f, config['batch_size'], strict=True)
@@ -33,4 +30,4 @@ tx = trainer(model_name    = config['model_name'],
              init_input        = jnp.ones([1, 28, 28, 1]),
              logdir = config['logdir'])
 
-tx.train(train_pipe, test_pipe, 2, len(train_pipe), 1)
+tx.train(train_pipe, test_pipe, 10, len(train_pipe), 1)
