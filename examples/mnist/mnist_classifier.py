12d01d77828648fc6b49dde667cd6c47bd40544d
luo9 <guanxiong.luo@med.uni-goettingen.de>
2024-01-10 17:05:47 +0100

    create mnist folder

--- examples/mnist/mnist_classifier.py
+++ examples/mnist/mnist_classifier.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/home/gluo/mitax')
+
 from mitax.trainer import trainer
 from mitax.misc import utils
 
@@ -8,12 +11,12 @@ import numpy as np
 import os
 os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES']='3'
-
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']='false'
 
 def map_f(x):
     return x[0][..., np.newaxis], x[1]
 
-config = utils.load_config('./mnist.yaml')
+config = utils.load_config('/home/gluo/mitax/examples/mnist.yaml')
 
 d1 = Mnist('train', True, './')
 train_pipe = utils.dataloader(d1, config['num_thread'], map_f, config['batch_size'], strict=True)
@@ -21,13 +24,13 @@ train_pipe = utils.dataloader(d1, config['num_thread'], map_f, config['batch_siz
 d2 = Mnist('test', True, './')
 test_pipe = utils.dataloader(d2, config['num_thread'], map_f, config['batch_size'], strict=True)
 
-tx = trainer(model_name    = config['model_name'],
-             model_hparams = {},
+tx = trainer(net_name    = config['net_name'],
+             net_hparams = {},
              loss_name     = config['loss_name'],
              loss_params  = {},
              optimizer_name = config['optimizer'],
              optimizer_hparams ={'lr': config['lr']}, 
-             init_input        = jnp.ones([1, 28, 28, 1]),
+             init_input        = {'x': jnp.ones([1, 28, 28, 1])},
              logdir = config['logdir'])
 
-tx.train(train_pipe, test_pipe, 10, len(train_pipe), 1)
+tx.train(train_pipe, test_pipe, 30, len(train_pipe), 1)
