4f7a7f44731084b2d21f484eec7da5aac5825934
luo9 <guanxiong.luo@med.uni-goettingen.de>
2024-01-15 15:51:31 +0100

    update classifier

--- mitax/model/classifier.py
+++ mitax/model/classifier.py
@@ -8,10 +8,11 @@ class cross_entropy():
 
     
     def __call__(self, apply_fn, params, batch, training):
-        imgs, labels = batch
+        imgs, labels, key = batch
         logits = apply_fn({'params': params},
                                     imgs,
-                                    train=training)
+                                    train=training,
+                                    rngs={'dropout': key} if training else None)
         loss = optax.softmax_cross_entropy_with_integer_labels(logits, labels).mean()
         accuracy = (logits.argmax(axis=-1) == labels).mean()
         return loss, {'accuracy': accuracy}
